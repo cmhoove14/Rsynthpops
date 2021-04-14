7a52fb8fe73db8a0c87c2167c8b0c5043c8b3e0e
cmhoove14 <cmhoove14@gmail.com>
2021-04-14 10:46:46 -0700

    Update data shipped with package: new lookup files to relate acs to pums data

--- R/data.R
+++ R/data.R
@@ -20,17 +20,70 @@
 
 #' Occupations codes
 #'
-#' Dataset to condense occupation codes into groups
+#' Dataset to relate occupational codes to acs variables
 #'
-#' @format A data frame with 74091 rows and 4 variables:
+#' @format A data frame with 563 rows and 6 variables:
 #' \describe{
 #'   \item{Code}{Specific codes corresponding to occupation}
 #'   \item{Code2}{Occupation group}
 #'   \item{Description}{Occupation description}
 #'   \item{d2_code}{Two digit occupation group}
-#'   \item{New_Code}{Code used to condense into groups}
+#'   \item{occp_match}{field used to merge pums and acs datasets. See `Get_Helpers.R`}
+#'   \item{acs_var}{acs variable code corresponding to number of individuals in that occupation in the census geography}
 #'   ...
 #' }
-#' @source \url{https://www2.census.gov/programs-surveys/acs/tech_docs/pums/code_lists/ACSPUMS2014_2018CodeLists.xlsx}
+#' @source \url{https://www2.census.gov/programs-surveys/acs/tech_docs/pums/code_lists/ACSPUMS2015_2019CodeLists.xlsx}
 #' 
-"OCC_list"
+"acs_occp_lookup"
+
+
+#' Age by sex lookup table
+#'
+#' Dataset to relate acs variables from B01001 tables (age by sex and age by sex by race/ethnicity) to age categories and sex
+#'
+#' @format A data frame with 298 rows and 6 variables:
+#' \describe{
+#'   \item{name}{acs variable name}
+#'   \item{label}{acs variable label}
+#'   \item{concept}{variable description}
+#'   \item{age_min}{minimum age captured in variable extracted from label}
+#'   \item{age_max}{maximum age captured in variable extracted from label}
+#'   \item{sex}{sex captured in variable extracted from label}
+#'   \item{sex_num}{numeric sex where Male=1, female=2}
+#'   ...
+#' }
+#' 
+"acs_sex_by_age_lookup"
+
+
+#' Race and ethnicity lookup table
+#'
+#' Dataset to relate acs variables from B03002 tables (race and ethnicity totals) to race and ethnicity categories
+#'
+#' @format A data frame with 18 rows and 3 variables:
+#' \describe{
+#'   \item{name}{acs variable name}
+#'   \item{hispanic}{binary of hispanic or latino (1) or not hispanic or latino (0)}
+#'   \item{race}{character description of race}
+#'   ...
+#' }
+#' 
+"acs_race_eth_lookup"
+
+
+
+#' Household income lookup table
+#'
+#' Dataset to relate acs variables from S1901_C01 tables (household income groups) to aid merging to pums income categories
+#'
+#' @format A data frame with 18 rows and 3 variables:
+#' \describe{
+#'   \item{name}{acs variable name}
+#'   \item{label}{acs variable label}
+#'   \item{concept}{variable description}
+#'   \item{income_min}{minimum income captured in variable extracted from label}
+#'   \item{income_max}{maximum income captured in variable extracted from label}
+#'   ...
+#' }
+#' 
+"acs_hhincome_lookup"
